daf6d8f233eb0817832c8f96a41bdf5b8024ee8c
bsgitt <boonnhon123@gmail.com>
2020-12-30 02:54:59 +0700

    add PrivateRoute AuthContext

--- src/App.js
+++ src/App.js
@@ -6,17 +6,24 @@ import {
 } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./components/todos";
+import { AuthProvider } from "./context/Auth/AuthContext";
 import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import PriveRoute from "./PrivateRoute";
 function App() {
   return (
     <>
+    <AuthProvider>
     <Router>
       <Switch>
-        <Route path='/dashboard' component={Dashboard}/>
+        <PriveRoute exact path='/dashboard' component={Dashboard}/>
+        <Route path='/signUp' component={SignUp}/>
+        <Route path='/signIn' component={SignIn}/>
         <Home/>
       </Switch>
     </Router>
-     
+    </AuthProvider>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom";
 import "./styles/main.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { UserProvider } from "./context/Auth/UserContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserProvider>
         <App />
-      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
