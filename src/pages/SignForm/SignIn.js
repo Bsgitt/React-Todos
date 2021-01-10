1ff8b82b373ba72365b892fe5c8fe779cdb9a4bb
bsgitt <Boonnhon123@gmail.com>
2021-01-10 21:15:59 +0700

    final

--- src/pages/SignForm/SignIn.js
+++ src/pages/SignForm/SignIn.js
@@ -10,29 +10,34 @@ function SignIn() {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const history = useHistory();
- async function handleLogin(e){
-        e.preventDefault()
-        try {
-          setError('')
-          setLoading(true)
-          await signIn(emailRef.current.value,passwordRef.current.value)
-          history.push('/dashboard')
-        } catch (error) {
-          setError('Failed to Login')
-        }
-        setLoading(false)
+  async function handleLogin(e) {
+    e.preventDefault();
+    try {
+      setError("");
+      setLoading(true);
+      await signIn(emailRef.current.value, passwordRef.current.value);
+      history.push("/dashboard");
+    } catch (error) {
+      setError("Failed to Login");
+    }
+    setLoading(false);
   }
   useEffect(() => {
     const unmount = true;
 
     return () => unmount;
   }, []);
-    return (
-        <div>
-            <StyledForm>
+  return (
+    <div>
+      <StyledForm>
         <form onSubmit={handleLogin}>
-          <input id='email' type="text" placeholder="Email" ref={emailRef} />
-          <input id='passsword' type="password" placeholder="Password" ref={passwordRef} />
+          <input id="email" type="text" placeholder="Email" ref={emailRef} />
+          <input
+            id="passsword"
+            type="password"
+            placeholder="Password"
+            ref={passwordRef}
+          />
           {error && (
             <div className="text-sm text-center text-red-500">{error}</div>
           )}
@@ -41,13 +46,12 @@ function SignIn() {
             <Link to="/forgot-password">Forgot Password?</Link>
           </div>
           <div className="w-64 text-center text-gray-400 mt-2">
-        Need an account? <Link to="/signup">Sign Up</Link>
-      </div>
-      
+            Need an account? <Link to="/signup">Sign Up</Link>
+          </div>
         </form>
       </StyledForm>
-        </div>
-    );
+    </div>
+  );
 }
 
 export default SignIn;
